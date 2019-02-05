acba0408454e84e6422bba5ad00573195d568a62
Luis Carlos de Souza <luis.muniz@gett.com.br>
2019-02-05 16:03:51 -0200

    feat(task_spec): added test model Task

---
+++ server/spec/controllers/tasks_controller_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe TasksController do
+  context '#create' do
+    xit 'when is successful' do
+    end
+
+    xit 'when is' do
+    end
+  end
+end
--- server/spec/models/task_spec.rb
+++ server/spec/models/task_spec.rb
@@ -4,12 +4,42 @@ require 'rails_helper'
 
 RSpec.describe Task do
   context '#save' do
-    it 'when is successful' do
-      expect(Task.new(description: 'description').save).to eql(true)
+    it 'save task successful' do
+      expect(Task.new(description: 'description', status: 0).save).to eql(true)
     end
 
     it 'when is' do
       expect(Task.new.save).to eql(false)
+    end
+  end
+
+  context '#destroy' do
+    it 'destroy ok' do
+      task = Task.all
+
+      task.each do |_t|
+        expect(task.destroy).to eql(true)
+      end
+    end
+  end
+
+  context '#update' do
+    it 'update description success' do
+      task = Task.create(description: 'description test', status: 0)
+      task.description = 'teste 2'
+      expect(task.save).to eql(true)
+    end
+
+    it 'update description fail' do
+      task = Task.create(description: 'description test', status: 0)
+      task.description = nil
+      expect(task.save).to eql(false)
+    end
+
+    it 'update status success' do
+      task = Task.create(description: 'description test', status: 0)
+      task.status = 1
+      expect(task.save).to eql(true)
     end
   end
 end
