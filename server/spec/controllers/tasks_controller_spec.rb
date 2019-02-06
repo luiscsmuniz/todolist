eb3f89048bb7990e57d6b4d6a11ffb18ab43429b
Luis Carlos de Souza <luis.muniz@gett.com.br>
2019-02-06 10:13:35 -0200

    feat(spec): added update+delete task_controller_spec

--- server/spec/controllers/tasks_controller_spec.rb
+++ server/spec/controllers/tasks_controller_spec.rb
@@ -5,35 +5,59 @@ require 'rails_helper'
 RSpec.describe TasksController, type: :request do
   context '#create' do
     it 'POST tasks create' do
-      post '/tasks.json', params: { task: { description: 'test', status: 0 } }
+      post '/tasks.json', params: { task: { description: 'test json', status: 0 } }
       puts response.body
       expect(response.content_type).to eq('application/json')
+      expect(response).to have_http_status(201)
     end
 
     it 'POST tasks fail' do
       post '/tasks.json', params: { task: { description: nil, status: 2 } }
       puts response.body
       expect(response.content_type).to eq('application/json')
+      expect(response).to have_http_status(422)
     end
   end
 
   context '#index' do
     it 'GET index' do
-      post '/tasks.json', params: { task: { description: 'test get json', status: 0 } }
+      post '/tasks.json', params: { task: { description: 'test json', status: 0 } }
       get '/tasks.json'
 
       puts JSON.parse(response.body, symbolize_names: true)
       expect(response.content_type).to eq('application/json')
+      expect(response).to have_http_status(200)
     end
   end
 
   context '#destroy' do
-    xit 'destroy task successful' do
+    it 'destroy task successful' do
+      post '/tasks.json', params: { task: { description: 'test json', status: 0 } }
+      count = Task.count
+      result = JSON.parse(response.body)
+      delete '/tasks/' + result['id'].to_s + '.json'
+      expect { count }.to change { Task.count }.by(0)
+      expect(response).to have_http_status(204)
     end
   end
 
   context '#update' do
-    xit 'update success' do
+    it 'update success' do
+      post '/tasks.json', params: { task: { description: 'test json', status: 0 } }
+      puts result = JSON.parse(response.body)
+      put '/tasks/' + result['id'].to_s + '.json', params: { task: { description: 'update test json', status: 1 } }
+      puts JSON.parse(response.body)
+      expect(response.content_type).to eq('application/json')
+      expect(response).to have_http_status(200)
+    end
+
+    it 'update fail' do
+      post '/tasks.json', params: { task: { description: 'test json', status: 0 } }
+      result = JSON.parse(response.body)
+      put '/tasks/' + result['id'].to_s + '.json', params: { task: { description: nil, status: nil } }
+      puts JSON.parse(response.body)
+      expect(response.content_type).to eq('application/json')
+      expect(response).to have_http_status(422)
     end
   end
 end
